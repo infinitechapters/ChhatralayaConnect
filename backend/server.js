fb5565b76ad1c9c971bfb72844101416f989f598
infinitechapters <khushiiyer14@gmail.com>
2026-05-14 14:52:39 +0530

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.VITE_URL || "http://localhost:3000",
   credentials: true
 }));
 app.use(express.json());
+
 
 /* Routes */
 app.use("/api/auth", authRoutes);
